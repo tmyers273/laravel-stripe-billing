f2c9cb27f41de5afda75323fb2e25811dac2a0e6
Denis Mitrofanov <denis.mitr@gmail.com>
2018-10-28 11:33:04 +0300

    refactors service provider

--- src/StripeBillingServiceProvider.php
+++ src/StripeBillingServiceProvider.php
@@ -15,6 +15,34 @@ class StripeBillingServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/stripe-billing.php' => config_path('stripe-billing.php'),
         ], 'config');
+
+        if ( ! class_exists('AddStripeBillingColumnsToOwnerTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/add_stripe_billing_columns_to_owner_table.php' =>
+                    database_path('migrations/' . date('Y_m_d_His', time()) . '_add_stripe_billing_columns_to_owner_table.php'),
+            ], 'migrations');
+        }
+
+        if ( ! class_exists('CreatePlansTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_plans_table.php' =>
+                    database_path('migrations/' . date('Y_m_d_His', time()) . '_create_plans_table.php'),
+            ], 'migrations');
+        }
+
+        if ( ! class_exists('CreatePricingPlansTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_pricing_plans_table.php' =>
+                    database_path('migrations/' . date('Y_m_d_His', time()) . '_create_pricing_plans_table.php'),
+            ], 'migrations');
+        }
+
+        if ( ! class_exists('CreateSubscriptionsTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_subscriptions_table.php' =>
+                    database_path('migrations/' . date('Y_m_d_His', time()) . '_create_subscriptions_table.php'),
+            ], 'migrations');
+        }
     }
 
     public function register()
@@ -23,34 +51,6 @@ class StripeBillingServiceProvider extends ServiceProvider
             __DIR__.'/../config/stripe-billing.php',
             'stripe-billing'
         );
-
-        if ( ! class_exists('AddStripeBillingColumnsToOwnerTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/add_stripe_billing_columns_to_owner_table.php' =>
-                    database_path('migrations/' . date('Y_m_d_His', time()) . '_1_add_stripe_billing_columns_to_owner_table.php'),
-            ], 'migrations');
-        }
-
-        if ( ! class_exists('CreatePlansTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_plans_table.php' =>
-                    database_path('migrations/' . date('Y_m_d_His', time()) . '_2_create_plans_table.php'),
-            ], 'migrations');
-        }
-
-        if ( ! class_exists('CreatePricingPlansTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_pricing_plans_table.php' =>
-                    database_path('migrations/' . date('Y_m_d_His', time()) . '_3_create_pricing_plans_table.php'),
-            ], 'migrations');
-        }
-
-        if ( ! class_exists('CreateSubscriptionsTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_subscriptions_table.php' =>
-                    database_path('migrations/' . date('Y_m_d_His', time()) . '_4_create_subscriptions_table.php'),
-            ], 'migrations');
-        }
 
         $this->app->bind('stripe-customer-gateway', function() {
             return new StripeCustomerGateway();
