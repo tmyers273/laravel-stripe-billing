07a7c254996607cace6c0ad25bba3eb7088c741c
Tom Myers <tmyers273@gmail.com>
2020-06-26 16:43:54 +1200

    Renames migration class names

--- database/migrations/create_products_table.php
+++ database/migrations/create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateStripeProductsTable extends Migration
 {
     /**
      * Run the migrations.
